b49cba9df4970d5efbfcb06b768c2708a2188f01
Fauzi Kurniawan <71652809+kurniawan26@users.noreply.github.com>
2025-04-04 13:21:23 +0700

    Refactor company page to use DetailPage components for improved structure and readability

--- src/app/company/[id]/page.tsx
+++ src/app/company/[id]/page.tsx
@@ -1,5 +1,7 @@
+import BodyContent from '@/components/Pages/DetailPage/BodyContent'
+import FooterContent from '@/components/Pages/DetailPage/FooterContent'
+import HeaderContent from '@/components/Pages/DetailPage/HeaderContent'
 import dataJson from '@/data/data.json'
-import Image from 'next/image'
 
 
 export default async function Page({
@@ -14,62 +16,13 @@ export default async function Page({
   }
 
   return (
-    <div className='min-md:px-[355px] -mt-6'>
-      <div className="flex items-center justify-between overflow-hidden bg-white border-gray-300 rounded-md dark:bg-midnight dark:border-midnight">
-        <div className="h-[140px] w-[140px] flex justify-center items-center" style={{ backgroundColor: post.logoBackground }}>
-          <Image src={`/${post.logo}`} alt="logo" width={64} height={64} />
-        </div>
-
-        <div className="flex justify-between w-full p-10">
-          <div className="flex flex-col gap-1">
-            <h2 className='font-bold text-midnight'>{post.company}</h2>
-            <p className='text-sm text-dark-gray'>{post.website}</p>
-          </div>
-          <button className="text-violet bg-[#eeeffc] font-bold px-4 h-[48px] rounded-md">
-            Company Site
-          </button>
-        </div>
+    <div className='w-full'>
+      <div className='-mt-6 sm:px-0 max-md:px-8 lg:px-30'>
+        <HeaderContent post={post} />
+        <BodyContent post={post} />
       </div>
 
-      <div className="flex flex-col gap-4 p-8 mt-6 bg-white border-gray-300 rounded-md dark:bg-midnight dark:border-midnight">
-        <div className="flex items-center justify-between">
-          <div className="flex flex-col gap-2">
-            <div className="flex gap-2 text-dark-gray">
-              <p className='text-dark-gray'>{post.postedAt}</p>
-              <span className="text-2xl">&bull;</span>
-              <p className='text-dark-gray'>{post.contract}</p>
-            </div>
-            <h3 className="font-bold text-very-dark-blue dark:text-white">
-              {post.position}
-            </h3>
-            <h4 className="font-bold text-violet">
-              {post.location}
-            </h4>
-          </div>
-
-          <button className="text-white bg-violet font-bold px-4 h-[48px] rounded-md">
-            Apply Now
-          </button>
-        </div>
-
-        <p className=''>{post.description}</p>
-
-        <h3 className='font-bold text-midnight'>Requirements</h3>
-        <p className=''>{post.requirements.content}</p>
-        <ul className='pl-6 list-disc marker:text-violet'>
-          {post.requirements.items.map((item, index) => (
-            <li key={index} className='text-dark-gray'>{item}</li>
-          ))}
-        </ul>
-
-        <h3 className='font-bold text-midnight'>What you will do</h3>
-        <p className=''>{post.role.content}</p>
-        <ul className='pl-6 list-disc marker:text-violet'>
-          {post.role.items.map((item, index) => (
-            <li key={index} className='text-dark-gray'>{item}</li>
-          ))}
-        </ul>
-      </div>
+      <FooterContent post={post} />
     </div>
   )
 }
